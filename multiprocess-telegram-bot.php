c99eb0012cbc7893b84a8bf75ce5c5083c989dbf
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-03-27 11:55:15 +0000

    Apply fixes from StyleCI

--- multiprocess-telegram-bot.php
+++ multiprocess-telegram-bot.php
@@ -10,18 +10,20 @@ set_time_limit(0);
 
 define('TOKEN', 'XXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 
-function apiRequest($method, $parameters = []) {
-  foreach ($parameters as $key => &$val) {
-    if (is_array($val)) {
-      $val = json_encode($val);
+function apiRequest($method, $parameters = [])
+{
+    foreach ($parameters as $key => &$val) {
+        if (is_array($val)) {
+            $val = json_encode($val);
+        }
     }
-  }
-  $ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
+    $ch = curl_init('https://api.telegram.org/bot'.TOKEN.'/'.$method);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
 }
 
 if (file_exists('updatesOffset')) {
@@ -31,30 +33,27 @@ if (file_exists('updatesOffset')) {
 $offset = @file_get_contents('updatesOffset');
 
 while (true) {
-
     $updates = apiRequest('getUpdates', ['offset' => $offset]);
     $updates = json_decode($updates, true);
     $updates = $updates['result'];
 
     foreach ($updates as $update) {
-        if($update['update_id'] > $offset) {
+        if ($update['update_id'] > $offset) {
             $offset = $update['update_id'] + 1;
         }
         $pid = pcntl_fork();
-        if($pid === -1) {
+        if ($pid === -1) {
             die();
-        }
-        elseif ($pid) {
+        } elseif ($pid) {
             // Created child with PID $pid
-        }
-        else {
-            if(isset($update['message'])) {
+        } else {
+            if (isset($update['message'])) {
                 $message = $update['message'];
                 $message_id = $message['message_id'];
                 $chat_id = $message['chat']['id'];
-                if(isset($message['text'])) {
+                if (isset($message['text'])) {
                     $text = $message['text'];
-                    if($text == '/start') {
+                    if ($text == '/start') {
                         apiRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Hi! I am a miltiprocess Telegram bot!', 'reply_to_message_id' => $message_id]);
                     }
                 }
@@ -64,5 +63,4 @@ while (true) {
     }
 
     file_put_contents('updatesOffset', $offset);
-
 }
